3f34268c5575dddcf22b2ee0f471d4721f773804
Timur Garipov <tr.garipov@yandex.ru>
2022-05-14 16:51:57 +0300

    2

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,17 +4,17 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        Operator operator1 = new Operator(1);
-        Operator operator2 = new Operator(2);
-        Operator operator3 = new Operator(3);
+        new Operator(1);
+        new Operator(2);
+        new Operator(3);
 
 
 
 
-        Customer customer1 = new Customer("Иван");
-        Customer customer2 = new Customer("Алексей");
-        Customer customer3 = new Customer("Егор");
-        Customer customer4 = new Customer("Петр");
+        new Customer("Иван");
+        new Customer("Алексей");
+        new Customer("Егор");
+        new Customer("Петр");
 
 
 
